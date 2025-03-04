85a75786fce07c2a35ea3d3168ebc9baef320462
Vitor Seredião <vitor.serediao@hotmail.com>
2025-03-04 10:29:48 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,12 +1,17 @@
-document.getElementById('idk').addEventListener('mouseover', function() {
-  const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-  const randomShadow = `0 4px 8px ${randomColor}`;
-  
-  this.style.backgroundColor = randomColor;
-  this.style.idk = randomShadow;
+document.getElementById("moveButton").addEventListener("click", () => {
+  const image = document.getElementById("movableImage");
+  const botao = document.getElementById('backbutton__kit');
+  const textoKit = document.getElementById('psychedelic');
+  image.style.left = "0%";
+  botao.style.left = '2%';
+  textoKit.style.left = '65%'; // Move the image to the left edge
 });
 
-document.getElementById('idk').addEventListener('mouseout', function() {
-  this.style.backgroundColor = '#ffffff';
-  this.style.idk = '0 4px 8px rgba(0, 0, 0, 0.2)';
+document.getElementById('backbutton__kit').addEventListener('click', () => {
+  const image = document.getElementById("movableImage");
+  const botao = document.getElementById('backbutton__kit');
+  const textoKit = document.getElementById('psychedelic');
+  image.style.left = "100%";
+  botao.style.left = '100%';
+  textoKit.style.left = '110%'
 })
